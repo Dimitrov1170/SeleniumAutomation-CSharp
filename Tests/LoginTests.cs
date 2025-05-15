e2b3ad1d2f5a5538d3716a613834099af24e3ec0
Georgi Dimitrov <georgi.dimitrov1170@gmail.com>
2025-05-15 14:57:29 +0300

    commit

--- Tests/LoginTests.cs
+++ Tests/LoginTests.cs
@@ -1,16 +1,49 @@
-namespace SeleniumAutomation
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumAutomation.Pages;
+
+namespace SeleniumAutomation.Tests
 {
-    public class Tests
+    public class LoginTests
     {
+        IWebDriver driver;
+        LoginPage loginPage;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            loginPage = new LoginPage(driver);
         }
 
         [Test]
-        public void Test1()
+        public void ValidLoginTest()
         {
-            Assert.Pass();
+            loginPage.EnterUsername("standard_user");
+            loginPage.EnterPassword("secret_sauce");
+            loginPage.ClickLogin();
+
+            Assert.That(driver.Url, Does.Contain("inventory"));
+        }
+
+        [Test]
+        public void InvalidLoginTest()
+        {
+            loginPage.EnterUsername("locked_out_user");
+            loginPage.EnterPassword("wrong_password");
+            loginPage.ClickLogin();
+
+            Assert.That(loginPage.GetErrorMessage(), Does.Contain("Epic sadface: Username and password do not match"));
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+            driver.Dispose();
         }
     }
 }
