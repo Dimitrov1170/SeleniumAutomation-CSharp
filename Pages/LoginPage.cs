8931987f3bf21750749ebcf94cadd455e2eccefb
Georgi Dimitrov <georgi.dimitrov1170@gmail.com>
2025-05-15 14:47:27 +0300

    Initial commit - Selenium Automation Project

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumAutomation.Pages
+{
+    public class LoginPage
+    {
+        private readonly IWebDriver driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        // Locators
+        private IWebElement UsernameField => driver.FindElement(By.Id("user-name"));
+        private IWebElement PasswordField => driver.FindElement(By.Id("password"));
+        private IWebElement LoginButton => driver.FindElement(By.Id("login-button"));
+        private IWebElement ErrorMessage => driver.FindElement(By.ClassName("error-message-container"));
+
+        // Actions
+        public void EnterUsername(string username) => UsernameField.SendKeys(username);
+        public void EnterPassword(string password) => PasswordField.SendKeys(password);
+        public void ClickLogin() => LoginButton.Click();
+
+        public string GetErrorMessage() => ErrorMessage.Text;
+    }
+}
